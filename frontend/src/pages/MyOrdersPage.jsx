cff9c4d121d500f62af225c06b115235d61d5c1b
Anh Khoa <anhkhoa392004@gmail.com>
2025-03-27 21:23:20 +0700

    finish user layout

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const MyOrdersPage = () => {
     const [orders, setOrders] = useState([]);
+    const navigate =useNavigate();
 
     useEffect(() => {
         // Simulate fetching orders
@@ -42,6 +44,10 @@ const MyOrdersPage = () => {
         }, 1000);
     }, []);
 
+    const handleRowClick=(orderId) =>{
+        navigate(`/order/${orderId}`)
+    };
+
     return (
         <div className="max-w-7xl mx-auto p-4 sm:p-6">
             <h2> My Orders</h2>
@@ -61,7 +67,7 @@ const MyOrdersPage = () => {
                     <tbody>
                         {orders.length > 0 ? (
                             orders.map((order) => (
-                                <tr key={order._id} className="border-b hover:border-gray-50 cursor-pointer">
+                                <tr key={order._id} onClick={() => handleRowClick(order._id)}    className="border-b hover:border-gray-50 cursor-pointer">
                                     <td className="py-2 px-2 sm:py-4 sm:px-4">
                                         <img
                                             src={order.orderItems[0].image}
