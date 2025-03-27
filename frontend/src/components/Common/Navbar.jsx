3756d802423f59b37879c752fcb37710d79de401
Anh Khoa <anhkhoa392004@gmail.com>
2025-03-27 13:28:22 +0700

    add filter side bar page collection

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -4,7 +4,7 @@ import {
     HiOutlineUser,
     HiOutlineShoppingBag,
     HiBars3BottomRight,
-  } from "react-icons/hi2";
+} from "react-icons/hi2";
 
 import SearchBar from './Searchbar';
 import CartDrawer from '../Layout/CartDrawer';
@@ -18,7 +18,7 @@ const Navbar = () => {
     const toggleNavDrawer = () => {
         setNavDrawerOpen(!navDrawerOpen);
     };
-    
+
     const toggleCartDrawer = () => {
         setDrawerOpen(!drawerOpen);
     };
@@ -29,13 +29,13 @@ const Navbar = () => {
                 {/* Left - Logo */}
                 <div>
                     <Link to="/" className="text-2xl font-medium">
-                        Maples 
+                        Maples
                     </Link>
                 </div>
                 {/* Center - Navigation Links */}
                 <div className="hidden md:flex space-x-6">
                     <Link
-                        to="#"
+                        to="/collections/all " 
                         className="text-gray-700 hover:text-black text-sm font-medium uppercase"
                     >
                         Men
@@ -67,30 +67,29 @@ const Navbar = () => {
                     <button onClick={toggleCartDrawer}
                         className='relative hover:text-black'
                     >
-                        <HiOutlineShoppingBag className='h-6 w-6 text-gray-700'/>
+                        <HiOutlineShoppingBag className='h-6 w-6 text-gray-700' />
                         <span className='absolute -top-1 bg-[#FF0000] text-white text-xs rounded-full px-2 py-0.5'>
                             4
                         </span>
                     </button>
                     {/* search  */}
                     <div className='overflow-hidden'>
-                        <SearchBar/>
+                        <SearchBar />
                     </div>
 
                     <button onClick={toggleNavDrawer} className='md:hidden'>
-                        <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
+                        <HiBars3BottomRight className='h-6 w-6 text-gray-700' />
                     </button>
-                </div>  
+                </div>
 
             </nav>
-            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+            <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
             {/* Mobile Navigation */}
             <div>
                 <div
                     className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:w-1/3 h-full bg-white shadow-lg transform
-                    transition-transform duration-300 z-50 ${
-                        navDrawerOpen ? "translate-x-0" : "-translate-x-full"
-                    }`}
+                    transition-transform duration-300 z-50 ${navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+                        }`}
                 >
                     <div className="flex justify-end p-4">
                         <button onClick={toggleNavDrawer}>
@@ -100,20 +99,20 @@ const Navbar = () => {
                     <div className='p-4'>
                         <h2 className='text-xl font-semibold mb-4'>Menu</h2>
                         <nav className='space-y-4'>
-                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
-                            Men
-                        </Link>
-                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
-                            Women
-                        </Link>
-                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
-                            Top Wear
-                        </Link>
-                        <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
-                            Bottom Wear
-                        </Link>
+                            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+                                Men
+                            </Link>
+                            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+                                Women
+                            </Link>
+                            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+                                Top Wear
+                            </Link>
+                            <Link to="#" onClick={toggleNavDrawer} className="block text-gray-600 hover:text-black">
+                                Bottom Wear
+                            </Link>
                         </nav>
-                        
+
                     </div>
                 </div>
             </div>
--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -119,7 +119,7 @@ const NewArrivals = () => {
 
   const scroll = (direction) => {
     const scrollAmount = direction === "left" ? -300 : 300;
-    scrollRef.current.scrollBy({ left: scrollAmount, behaviour: "smooth" });
+    scrollRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
   };
   // Update Scroll
   const updateScrollButtons = () => {
@@ -169,7 +169,7 @@ const NewArrivals = () => {
       onMouseLeave={handleMouseUpOrLeave}
     >
       {newArrivals.map((product) => (
-        <div key={product.id} className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative">
+        <div key={product._id} className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative">
           <img src={product.images[0]?.url}
             alt={product.images[0]?.altText || product.name}
             className="w-full h-[500px] object-cover rounded-lg"
@@ -177,7 +177,7 @@ const NewArrivals = () => {
 
           />
           <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
-            <Link to={`/product/${product.id}`} className="block">
+            <Link to={`/product/${product._id}`} className="block">
               <h4 className="font-medium">{product.name}</h4>
               <p className="mt-1">${product.price}</p>
             </Link>
