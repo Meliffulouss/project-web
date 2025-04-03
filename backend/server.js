e38b1e80c68232d6c5dc36550c1f4734c06fd949
Anh Khoa <anhkhoa392004@gmail.com>
2025-04-03 20:39:08 +0700

    update cart

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+// const checkoutRoutes = require("./routes/checkoutRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -21,6 +23,9 @@ app.get("/", (req, res) => {
 // api routes
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+// app.use("/api/checkout", checkoutRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
