b42aaaa29b623f6e68d5d2a5088c5512c2a8b6a4
Anh Khoa <anhkhoa392004@gmail.com>
2025-04-07 22:50:52 +0700

    update code

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
 
           <p className="mt-6 text-center text-sm">
             Don't have an account?
-            <Link to={`register?redirect=${encodeURIComponent(redirect)}`} className="text-blue-500">
+            <Link to={`/register?redirect=${encodeURIComponent(redirect)}`} className="text-blue-500">
               Register
             </Link>
           </p>
